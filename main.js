4f9c35c8d84c38ecb0986c8ce3116060892bbfac
yehuda-sanay <yehudasanay1@gmail.com>
2022-07-14 22:46:37 +0300

    14.07.22

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+const VehicleArray=[Vehicle.enginecapacity];
 class Vehicle{
     constructor(wheelsnumber,enginecapacity,color){
         this.wheelsnumber=wheelsnumber;
         this.enginecapacity=enginecapacity;
         this.color=color;
         this.vehicleDitels()
+    }
+    static bigEngine(){
+        return Math.max(...VehicleArray);
     }
     vehicleDitels(){
         return `${this.wheelsnumber}${this.enginecapacity}${this.color}` 
@@ -35,4 +39,23 @@ class Truck extends Vehicle{
     vehicleDitels(){
         return `${this.type}${super.vehicleDitels()}`
     }
-}
+    printTipe(){
+        let letrf=this.type.substring(0,1).toUpperCase();
+        let midel=this.type.substring(1,this.type.length-1);
+        let letrs=this.type.substring(this.type.length-1).toUpperCase();
+        return `${letrf}${midel}${letrs}`
+    }
+    get printPri(){
+        `${this.printTipe}`
+    }
+}console.log(Truck.printPri)
+
+
+
+// let stext="yehuda";
+// let sp=stext.substring(0,1).toUpperCase();
+// let ps=stext.substring(stext.length-1).toUpperCase();
+// let pps=stext.substring(1,stext.length-1);
+// console.log(pps);
+// console.log(sp);
+// console.log(ps);
